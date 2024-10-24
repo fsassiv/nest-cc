c4128c2ff12ac781f201e17557c1f237afd4d5bc
Flavio Andrade <flavio.ssiv@gmail.com>
2024-10-24 17:09:50 -0300

    try add pgAdmin to docker-compose file

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
+import { BookmarkModule } from './bookmark/bookmark.module';
+import { UserModule } from './user/user.module';
 
+ConfigModule.forRoot();
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [AuthModule, UserModule, BookmarkModule],
 })
 export class AppModule {}
